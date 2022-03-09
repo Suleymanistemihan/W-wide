d1a9312ec4751843291e321d17fa04b0cea75195
Suleymanistemihan <86014246+Suleymanistemihan@users.noreply.github.com>
2022-03-09 21:09:11 +0300

    master

--- src/component/screens/weatherScreen/Countries/istanbul.js
+++ src/component/screens/weatherScreen/Countries/istanbul.js
@@ -11,7 +11,11 @@ function WeatherIstanbul() {
   var day = date.getDate(dateData);
   day = day > 9 ? day : "0" + day;
   var minutes = date.getMinutes(dateData);
+  var hour = date.getHours(dateData)
+  hour = hour > 9 ? hour : "0" + hour
   minutes = minutes > 9 ? minutes : '0' + minutes;
+
+  const image = state1?.current?.condition?.icon
 
   const dispatch = (useDispatch())
   useEffect(() => {
@@ -20,13 +24,21 @@ function WeatherIstanbul() {
     []
   )
   return (
-    <View style={{ flex: 1, backgroundColor: "grey", justifyContent: "center", alignItems: "center" }}>
-      <View style={{ height: "75%", width: "75%", backgroundColor: "red", borderRadius: 20, alignItems: "center", justifyContent: "center" }}>
-        <Text style={{ fontSize: 25, color: "white", fontWeight: "bold", }}>{state1?.location?.region}</Text>
-        <Text style={{ fontSize: 15, color: "white", fontWeight: "bold", }}>{state1?.current?.temp_c}°C </Text>
-        <Text style={{ fontSize: 10, color: "white", fontWeight: "bold", }}>{state1?.current?.condition?.text}</Text>
-        <Text style={{ fontSize: 10, color: "white", fontWeight: "bold", }}>{`${day}.${date.getMonth()}.${date.getFullYear()}`}</Text>
-      </View>
+    <View style={{ flex: 1, backgroundColor: "#E3E5E4", justifyContent: "center", alignItems: "center" }}>
+      <ImageBackground source={require("../../../icon/Backgrounds/Galata.jpg")} style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center" }}>
+        <View style={{ height: "75%", width: "75%", backgroundColor: 'rgba(52, 52, 52, 0.8)', borderRadius: 20, alignItems: "center", justifyContent: "center", opacity: 0.8 }}>
+          <View style={{ marginBottom: 80, alignItems: "center" }}>
+            <Text style={{ fontSize: 45, color: "white", fontWeight: "bold", }}>{state1?.location?.region}</Text>
+            <Text style={{ fontSize: 40, color: "white" }}>{`${hour}:${minutes}`}</Text>
+            <Text style={{ fontSize: 35, color: "white" }}> {`${day}.${date.getMonth()}.${date.getFullYear()}`}</Text>
+          </View>
+          <View style={{ marginBottom: 50 }}>
+            <Text style={{ fontSize: 30, color: "white", fontWeight: "bold", }}>{state1?.current?.temp_c}°C </Text>
+          </View>
+          <Text style={{ fontSize: 20, color: "white", fontWeight: "bold", }}>{state1?.current?.condition?.text}</Text>
+          <Image style={{ width: 50, height: 50 }} source={{ uri: image }} />
+        </View>
+      </ImageBackground>
     </View >
   )
 }
--- src/component/screens/weatherScreen/Countries/izmir.js
+++ src/component/screens/weatherScreen/Countries/izmir.js
@@ -11,6 +11,8 @@ function WeatherIzmir() {
   var day = date.getDate(dateData);
   day = day > 9 ? day : "0" + day;
   var minutes = date.getMinutes(dateData);
+  var hour = date.getHours(dateData)
+  hour = hour > 9 ? hour : "0" + hour
   minutes = minutes > 9 ? minutes : '0' + minutes;
 
   const dispatch = (useDispatch())
@@ -20,13 +22,23 @@ function WeatherIzmir() {
     []
   )
   return (
-    <View style={{ flex: 1, backgroundColor: "grey", justifyContent: "center", alignItems: "center" }}>
-      <View style={{ height: "75%", width: "75%", backgroundColor: "red", borderRadius: 20, alignItems: "center", justifyContent: "center" }}>
-        <Text style={{ fontSize: 55, color: "white", fontWeight: "bold", }}>{state2?.location?.region}</Text>
-        <Text style={{ fontSize: 15, color: "white", fontWeight: "bold", }}>{state2?.current?.temp_c}°C </Text>
-        <Text style={{ fontSize: 10, color: "white", fontWeight: "bold", }}>{state2?.current?.condition?.text}</Text>
-        <Text style={{ fontSize: 10, color: "white", fontWeight: "bold", }}>{`${day}.${date.getMonth()}.${date.getFullYear()}`}</Text>
-      </View>
+    <View style={{ flex: 1, backgroundColor: "#E3E5E4", justifyContent: "center", alignItems: "center" }}>
+      <ImageBackground source={require("../../../icon/Backgrounds/izmir_saat_kulesi.jpg")} style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center" }}>
+        <View style={{ height: "75%", width: "75%", backgroundColor: 'rgba(52, 52, 52, 0.8)', borderRadius: 20, alignItems: "center", justifyContent: "center", opacity: 0.8 }}>
+          <View style={{ marginBottom: 80, alignItems: "center" }}>
+            <Text style={{ fontSize: 45, color: "white", fontWeight: "bold", }}>{state2?.location?.region}</Text>
+            <Text style={{ fontSize: 40, color: "white" }}>{`${hour}:${minutes}`}</Text>
+            <Text style={{ fontSize: 35, color: "white" }}> {`${day}.${date.getMonth()}.${date.getFullYear()}`}</Text>
+          </View>
+          <View style={{ marginBottom: 50 }}>
+            <Text style={{ fontSize: 30, color: "white", fontWeight: "bold", }}>{state2?.current?.temp_c}°C </Text>
+          </View>
+          <Text style={{ fontSize: 20, color: "white", fontWeight: "bold", }}>{state2?.current?.condition?.text}</Text>
+          <View style={{ marginBottom: 50 }}>
+            <Text style={{ fontSize: 15, color: "white", fontWeight: "bold", }}>{`${day}.${date.getMonth()}.${date.getFullYear()}`}</Text>
+          </View>
+        </View>
+      </ImageBackground>
     </View >
   )
 }
